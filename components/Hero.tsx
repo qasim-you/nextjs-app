7e8a6e365d80cbc3d3c1a1a0fd344c96cf4d0d1d
Qasim <qasimyousaf094@gmail.com>
2024-06-28 18:01:31 +0500

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
           <TextGenerateEffect
             words="Transforming Concepts into Seamless User Experiences"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl  "
+            className="text-center text-[40px] md:text-5xl lg:text-6xl text-white-100 "
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white">
             Hi! I&apos;m Qasim, a Next.js Developer based in Pakistan.
