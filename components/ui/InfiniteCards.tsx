c2477a3202b4e44f48eb0a1f979b9f99e8fe7989
Qasim <qasimyousaf094@gmail.com>
2024-06-28 22:04:38 +0500

    App Done

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { cn } from "@/utils/cn";
+
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import profilepic from "../../public/profile.jpg";
 export const InfiniteMovingCards = ({
   items,
   direction = "left",
@@ -115,18 +118,13 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 {/* add this div for the profile img */}
                 <div className="me-3">
-                  <img
-                    src="/profile.JPG"
-                    alt="profile"
-                    className=" w-50 h-24 rounded-full"
-                  />
-                  {/* <Image
-                    src="/profile.JPG"
+                  <Image
+                    src={profilepic}
                     width={50}
                     height={24}
                     className="rounded-full aspect-square object-cover"
                     alt="Picture of the author"
-                  /> */}
+                  />
                 </div>
                 <span className="flex flex-col gap-1">
                   {/* change text color, font-normal to font-bold, text-xl */}
